ed9a3e02966ec27d8e3ad57e2d4f8eb2a5316669
te-ing <hi2177@naver.com>
2022-01-25 14:04:05 +0900

    study: 구현 level2 조이스틱 해결

--- level2/조이스틱.js
+++ level2/조이스틱.js
@@ -7,8 +7,8 @@ const solution = (name) => {
     else answer += (91 - code);
   }
   
-  const indexA = name.split('').map((s, index) => s === "A" ? "A" : "S");
-  if (!indexA.includes("S")) return answer;
+  const names = name.split('').map((s, index) => s === "A" ? "A" : "S");
+  if (!names.includes("S")) return answer;
 
   const moveR = (i, arr) => {
     let cnt = i-1;
@@ -33,20 +33,15 @@ const solution = (name) => {
     return cnt
   }
   const moves = [];
-  for (let i = 0; i < indexA.length; i++) {
-    moves.push(moveL(i,[...indexA]))
-    moves.push(moveR(i,[...indexA]))
+  for (let i = 0; i < names.length; i++) {
+    moves.push(moveL(i,[...names]))
+    moves.push(moveR(i,[...names]))
+  }
+
+  for (let i = 0; i < names.length; i++) {
+    moves.push(moveL(i,[...names.reverse()])+1)
+    moves.push(moveR(i,[...names.reverse()])+1)
   }
 
   return answer + Math.min(...moves)
 }
-
-// console.log(solution("AAA")); // 0
-// console.log(solution("JEROENA")); // 5
-// console.log(solution("JANIFAA")); // 4     0,1,2,3,4 4 4 // 0,6,5,4,3,2,1
-// console.log(solution("JAN")); // 1
-// console.log(solution("HANIY")); // 3
-// console.log(solution("MUYAAAAAHO")); // 6
-// console.log(solution("MUYAAAHO")); // 6
-// 01234
-// 10111
