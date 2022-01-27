42eae11f194d1ca00b2dcb741cb28ca2ec461a83
te-ing <hi2177@naver.com>
2022-01-27 22:22:40 +0900

    study:  level2 메뉴리뉴얼 문제

---
+++ level2/메뉴리뉴얼.js
@@ -0,0 +1,20 @@
+function solution(orders, course) {
+  let answer = [];
+
+
+  return answer;
+}
+
+console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]));
+//	["AC", "ACDE", "BCFG", "CDE"]
+
+/**
+ * 코스요리 메뉴는 최소 2가지 이상의 단품 메뉴
+ * 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합만 메뉴 후보
+ * 
+ */
+
+// console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]));
+// // ["ACD", "AD", "ADE", "CD", "XYZ"]
+// console.log(solution(["XYZ", "XWY", "WXA"], [2, 3, 4])); // ["WX", "XY"]
+
