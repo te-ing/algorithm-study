347a07f6719e42ad0c7607856aa189cfdfa9ee5d
Taejung Kim <hi2177@naver.com>
2022-01-30 22:27:07 +0900

    study:  level1 소수찾기 풀이

---
+++ level1/소수찾기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+  let answer = [];
+  
+  function isPrime (n) {
+    for(let x of answer) {
+      if(x>Math.sqrt(n))return true
+      if(Number.isInteger(n/x)) return false
+    }
+    return true
+  }
+  
+  for(let i=2; i<=n; i++) {
+		if(!i%2) continue
+    if(isPrime(i)) answer.push(i)
+  }
+  
+  return answer.length;
+}
