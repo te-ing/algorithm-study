6ddb3457e0df57b35932fae3ceae715d9814c3cc
te-ing <hi2177@naver.com>
2022-02-03 14:30:08 +0900

    study:  level2 소수찾기 풀이

--- level2/소수찾기.js
+++ level2/소수찾기.js
@@ -1,18 +1,39 @@
 function solution(numbers) {
-  var answer = 0;
-  const primes = [];
+  let answer = 0;
+  const primes = new Set();
+  const check = Array.from({ length: numbers.length }, () => 0);
 
+  function DFS(s, L, check) {
+    if (L === numbers.length) {
+      s && primes.add(Number(s));
+      return
+    } else {
+      for (let i = 0; i < numbers.length; i++) {
+        if (!check[i]) {
+          check[i] = 1;
+          DFS(s+numbers[i], L+1, check)
+          DFS(s, L+1, check)
+          check[i] = 0;
+        }
+      }
+    }
+  }
+  DFS("", 0, check);
+  
   function isPrime(n) {
-    for (let i = 2; i < Math.sqrt(n); i++) {
-      if (!n % i) {
+    if(n<=1) return false
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (Number.isInteger(n/i)) {
         return false
       }
     }
-    answer++;
     return true;
   }
 
-
+  for (let x of primes) {
+    isPrime(x) && answer++;
+  }
+  
   return answer;
 }
 
