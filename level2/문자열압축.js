ad41b1ffe17ba787f437a16e54a3877206d83bb9
te-ing <hi2177@naver.com>
2022-02-09 15:24:22 +0900

    study:  level2 문자열 압축하기 풀이

--- level2/문자열압축.js
+++ level2/문자열압축.js
@@ -1,43 +1,25 @@
 function solution(s) {
-  let answer = [];
-  let cnt = 1;
+  let answer = [s.length];
   for (let i = 1; i <= s.length / 2; i++) {
     let words = "";
     let _s = s.slice();
-    while (_s.length > i) {
-      let word = _s.substring(0, i);
-      _s = _s.substring(i);
-      if (word === _s.substring(0, i)) {
+    while (_s.length) {
+      let cnt = 1;
+      let word = ""; 
+      while (_s.length >= i * cnt &&
+        _s.substring(0, i) === _s.substring(i * cnt, i * (cnt + 1))) {
+        word = _s.substring(0, i);
         cnt++;
-      } else if (word !== _s.substring(0, i)){
-        if (cnt === 1) {
-          words += word;  
-        } else {
-          words += (cnt + word);
-        }
-        cnt = 1;
       }
-    }
-    if (cnt === 1) {
-      words += _s;  
-    } else {
-      words += (cnt + _s);
+      if (cnt > 1) {
+        words += cnt + word;
+        _s = _s.substring(i*cnt);
+      } else {
+        words += _s.substring(0,i);
+        _s = _s.substring(i);
+      }
     }
     answer.push(words.length)
   }
   return Math.min(...answer);
 }
-
-console.log(solution("aababacdacd")); //aabab2acd 9
-// console.log(solution("aabbaccc")); // 7
-// console.log(solution("abcabcdede")); // 8 
-// console.log(solution("ababcdcdababcdcd")); // 9
-// console.log(solution("xababcdcdababcdcd")); // 17
-// console.log(solution("abcabcabcabcdededededede")); // 14 
-
-/**
-"ababcdcdababcdcd"	9
-"abcabcdede"	8
-"abcabcabcabcdededededede"	14
-"xababcdcdababcdcd"	17
- */
