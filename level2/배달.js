6b932fa7dd671292ccf391081803032f0c0df272
te-ing <hi2177@naver.com>
2022-04-23 18:39:47 +0900

    study:  level2 배달 추가풀이 수정

--- level2/배달.js
+++ level2/배달.js
@@ -27,31 +27,31 @@ function solution(N, road, K) {
 }
 
 const dfsSolution = (N, road, K) => {
-  const graph = [...Array(N + 1)].map((m) => []);
+  let answer = 0;
+  const graph = Array.from({ length: N + 1 }, () => []);
   road.forEach((r) => {
     graph[r[0]].push([r[1], r[2]]);
     graph[r[1]].push([r[0], r[2]]);
   });
-
-  const weight = new Array(graph.length).fill(Infinity);
   
-  const dfs = function (start, w) {
+  const weight = Array.from({ length: N + 1 }, () => Infinity);
+  
+  function DFS (start, w) { // w는 거리
     if (weight[start] < w) {
       return;
     } else {
       weight[start] = w;
-      for (let item of graph[start]) {
-        const [a, b] = item;
-        dfs(a, w + b);
+      for (let [a, b] of graph[start]) {
+        DFS(a, w + b);
       }
     }
   };
+  
+  DFS(1, 0);
+  weight.map(w => w <= K && answer++);
 
-  dfs(1, 0);
-
-
-  return weight.filter((w) => w <= K).length;
+  return answer
 };
 
-console.log(solution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4));
-console.log(dfsSolution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4));
+// console.log(solution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4));
+// console.log(dfsSolution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4));
