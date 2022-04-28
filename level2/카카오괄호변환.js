a362aa2b124786ff32170f2a76f7798810f54a4f
Taejung Kim <hi2177@naver.com>
2022-04-28 13:59:50 +0900

    study: level2 카카오괄호변환 풀이

--- level2/카카오괄호변환.js
+++ level2/카카오괄호변환.js
@@ -1,59 +1,57 @@
-function checkBalance(s) {
-  if (!s) return;
-  let left = 0;
-  let right = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    s[i] === "(" ? left++ : right++;
-    if (left !== 0 && left === right) {
-      return ++i
-    }
-  }
-}
-
-function checkCorrect(s) {
-  let correct = true;
-  if(!s) return correct
-  const stack = [s[0]];
-  
-  for (let i = 1; i < s.length; i++){
-    if(stack[stack.length-1] === "(" && s[i] === ")") {
-      stack.pop();
-    } else {
-      stack.push(s[i]);
-    }
-    if(stack.length) correct = false;
-    return correct
-  }
-
 function solution(p) {
-  let answer = '';
-  // console.log(checkBalance(p))
-  let balanceIndex = checkBalance(p);
-  // if(checkCorrect(p.slice(0,balanceIndex))){
-  //   answer += p.slice(0,balanceIndex);
-  // } else{
-  //   재귀함수
-  // }
-  if(p.slice(checkBalance(p)))
+  if (!p) return
 
-  function changeP(u, v) {
-    let result = "(";
-    result += v;
-    result += ")";
+  function balance(s) {
+    let left = 0;
+    let right = 0;
+    for (let i = 0; i < s.length-1; i++){
+      if (s[i] === "(") left++;
+      else right++;
+      if (left === right) return [s.slice(0, i + 1), s.slice(i+1)];
+    }
+    return [s]
   }
-  console.log("p", p.slice(1,-1).split('').reverse().join('')) 
-  console.log("p", p)
 
-  console.log(p.slice(0,balanceIndex));
-  console.log(p.slice(balanceIndex));
+  function correct(s) {
+    const queue = [s[0]];
+    for (let i = 1; i < s.length; i++){
+      if (queue[queue.length - 1] === "(" && s[i] === ")") {
+        queue.pop();
+      } else {
+        queue.push(s[i])  
+      }
+    }
+    if (queue.length) return false
+    else return true
+  }
 
+  function formatReverse(s) {
+    let result = "";
+    for (let x of s.slice(1, -1)) {
+      if (x === "(") result += ")"
+      else result += "("
+    }
+    return result
+  }
 
-  return answer;
+  function formatting(p) {
+    if(!p) return ""
+    let result = ""
+    const arr = balance(p);
+      if (correct(arr[0])) {
+        result += arr[0] + formatting(arr[1]);
+      } else {
+        result += "(";
+        result += formatting(arr[1]);
+        result += ")";
+        result += formatReverse(arr[0]);
+    }
+    return result
+  }
+    
+  return formatting(p);
 }
 
-
-
-// console.log(solution("(()())()")) //   "(()())()"
-// console.log(solution(")(")) // "()"
-console.log(solution("()))((()")) //   "()(())()"
+console.log(solution("(()())()")) //   "(()())()"
+console.log(solution(")(")) // "()"
+console.log(solution("()))((()")) //   "()(())()" 
