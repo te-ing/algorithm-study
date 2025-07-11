c999ba1fedc5a31cb261004451138896b6036082
te-ing <hi2177@naver.com>
2025-07-11 10:36:54 +0900

    백준: update template.js

--- algorithm/백준/template.js
+++ algorithm/백준/template.js
@@ -1,11 +1,17 @@
+// https://www.acmicpc.net/problem/1652
 const fs = require('fs');
 const stdin = (process.platform === 'linux'
   ? fs.readFileSync('/dev/stdin').toString()
-: ``
+  : `5
+....X
+`
 ).split('\n');
-
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
+
+// const [N, M, V] = input().split(' ').map(Number);
+// const N = +input();
+// const grid = Array.from({ length: N }, () => input().split(""));
